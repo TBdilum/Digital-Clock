44c5eecb31bcdf435cf5ca3eb5ac3cd583e19f74
Dilum paranavithana <dilumchandupa@gmail.com>
2024-11-11 23:13:23 +0530

    Finished codng the digital live clock.

--- src/Digitalclock.jsx
+++ src/Digitalclock.jsx
@@ -1,8 +1,39 @@
+import { useState,useEffect } from "react"
+
 export default function DigitalClock(){
+    
+    const [time,setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000 )
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    },[]);
+
+    function formatTime() {
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const merideum = hours > 12 ? 'PM' : 'AM';
+        
+        hours = hours % 12 || 12;
+
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${merideum}`
+    }
+
+    function padZero(number) {
+        return(number < 10 ?  "0" : "") + number;
+    }
+
+
     return(
       <div className="clock-container">
         <div className="clock">
-          <span>00:00:00</span>
+          <span>{formatTime()}</span>
         </div>
       </div>
     )
